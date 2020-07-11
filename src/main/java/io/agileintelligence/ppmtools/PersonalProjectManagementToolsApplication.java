d830d782f494c74e07e7780ef2601112122e4c9d
Shweta Yadav <yshweta@amazon.com>
2020-07-11 12:15:44 +0530

    First commit

---
+++ src/main/java/io/agileintelligence/ppmtools/PersonalProjectManagementToolsApplication.java
@@ -0,0 +1,13 @@
+package io.agileintelligence.ppmtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalProjectManagementToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProjectManagementToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/agileintelligence/ppmtools/PersonalProjectManagementToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.agileintelligence.ppmtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProjectManagementToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
